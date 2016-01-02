743241b0a0b0dbf97db31e817d8ec6f4a81464b8
Reza Ghorbani <r.ghorbani.f@gmail.com>
2016-01-02 10:01:05 +0330

    fix bugs

--- index.ios.js
+++ index.ios.js
@@ -2,11 +2,11 @@
 
 var React = require('react-native');
 var {
-    PropTypes,
-    StyleSheet,
-    requireNativeComponent,
-    View,
-    processColor,
+  PropTypes,
+  StyleSheet,
+  requireNativeComponent,
+  View,
+  processColor,
 } = React;
 
 /** A native reference to the chart view */
@@ -17,49 +17,55 @@ var RNChartView = requireNativeComponent('RNChartView', RNChart);
 
 /** A base styles object that the user can override by passing in their own styles */
 var styles = StyleSheet.create({
-    base: {}
+  base: {}
 });
 
 /** Our bridge component */
 var RNChart = React.createClass({
-    propTypes: {
-        // TODO: define what these objects look like
-        chartData: PropTypes.array.isRequired,
-        xLabels: PropTypes.array.isRequired,
-        animationDuration: PropTypes.number,
-        showGrid: PropTypes.bool,
-        verticalGridStep: PropTypes.number,
-        // TODO: allow strings and use processColor to convert
-        gridColor: PropTypes.number,
-        gridLineWidth: PropTypes.number,
-        showAxis: PropTypes.bool,
-        showXAxisLabels: PropTypes.bool,
-        showYAxisLabels: PropTypes.bool,
-        axisLineWidth: PropTypes.number,
-        labelFontSize: PropTypes.number,
-        // TODO: allow strings and use processColor to convert
-        labelTextColor: PropTypes.number
-    },
+  propTypes: {
+    // TODO: define what these objects look like
+    chartData: PropTypes.array.isRequired,
+    xLabels: PropTypes.array.isRequired,
+    animationDuration: PropTypes.number,
+    showGrid: PropTypes.bool,
+    verticalGridStep: PropTypes.number,
+    gridColor: PropTypes.string,
+    gridLineWidth: PropTypes.number,
+    showAxis: PropTypes.bool,
+    showXAxisLabels: PropTypes.bool,
+    showYAxisLabels: PropTypes.bool,
+    axisLineWidth: PropTypes.number,
+    labelFontSize: PropTypes.number,
+    labelTextColor: PropTypes.string
+  },
 
-    /** Pass the props to the native component */
-    setNativeProps(props) {
-        this.refs[CHART_REF].setNativeProps(props);
-    },
+  /** Pass the props to the native component */
+  setNativeProps(props) {
+    this.refs[CHART_REF].setNativeProps(props);
+  },
 
-    /** Render the native component with the correct props */
-    render() {
-        var style = [styles.base, this.props.style];
-        var nativeProps = Object.assign({}, this.props);
-        nativeProps.style = style;
-        for (var i = 0; i < nativeProps.chartData.length; i++) {
-            if (nativeProps.chartData[i].color)
-                nativeProps.chartData[i].color = processColor(nativeProps.chartData[i].color);
-            if (nativeProps.chartData[i].fillColor)
-                nativeProps.chartData[i].fillColor = processColor(nativeProps.chartData[i].fillColor);
-        };
+  /** Render the native component with the correct props */
+  render() {
+    var style = [styles.base, this.props.style];
+    var nativeProps = Object.assign({}, this.props);
 
-        return <RNChartView ref={CHART_REF} {...nativeProps} />;
+    nativeProps.style = style;
+    if (nativeProps.gridColor) {
+      nativeProps.gridColor = processColor(nativeProps.gridColor);
     }
+    if (nativeProps.labelTextColor) {
+      nativeProps.labelTextColor = processColor(nativeProps.labelTextColor);
+    }
+
+    for (var i = 0; i < nativeProps.chartData.length; i++) {
+      if (nativeProps.chartData[i].color)
+        nativeProps.chartData[i].color = processColor(nativeProps.chartData[i].color);
+      if (nativeProps.chartData[i].fillColor)
+        nativeProps.chartData[i].fillColor = processColor(nativeProps.chartData[i].fillColor);
+    };
+
+    return <RNChartView ref={CHART_REF} {...nativeProps} />;
+  }
 });
 
 module.exports = RNChart;
